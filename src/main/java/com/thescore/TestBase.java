027e24ca64aa01db3bb4f483a1832245155885da
Bhavik009 <bhavik@3squaretechnolabs.com>
2024-02-01 17:21:04 -0500

    feat: adding TestBase for reusability

--- src/main/java/com/thescore/TestBase.java
+++ src/main/java/com/thescore/TestBase.java
@@ -1,34 +1,17 @@
 package com.thescore;
 
-import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class testStandings {
-    /*
-    UiAutomator2Options options = new UiAutomator2Options()
-            .setUdid("123456")
-            .setApp("src/main/resources/app/thescore.apk");
-    AndroidDriver driver = new AndroidDriver(
-            new URL("http://127.0.0.1:4723"), options
-    );
-
-    public testStandings() throws MalformedURLException {
-    }*/
-
-    private AndroidDriver driver;
-
-
-    @BeforeTest
+public class TestBase {
+    protected AndroidDriver driver;
+    @BeforeClass
     public void setUp() throws MalformedURLException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("appium:deviceName", "Bhavik's S23");
@@ -42,13 +25,7 @@ public class testStandings {
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), desiredCapabilities);
     }
 
-    @Test
-    public void sampleTest() throws InterruptedException {
-        Thread.sleep(3000);
-
-    }
-
-    @AfterTest
+    @AfterClass
     public void tearDown() {
         driver.quit();
     }
---
+++ src/test/java/com/thescore/StandingsTest.java
@@ -0,0 +1,12 @@
+package com.thescore;
+
+import org.testng.annotations.Test;
+
+public class StandingsTest extends TestBase{
+
+    @Test
+    public void sampleTest() throws InterruptedException {
+        Thread.sleep(3000);
+    }
+
+}
